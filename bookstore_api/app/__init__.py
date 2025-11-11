ae284e75e1f4de1cbd868171f8a7c097b0a296e3
Oluwaseun Ilesanmi <seunowonikoko@gmail.com>
2025-11-11 13:33:53 -0500

    Setup route and flask-restful

--- bookstore_api/app/__init__.py
+++ bookstore_api/app/__init__.py
@@ -3,17 +3,22 @@ import os
 from flask import Flask
 from dotenv import load_dotenv
 from .extensions import db, migrate, ma
+
 # Import config
 from .config import DevelopmentConfig, ProductionConfig, TestConfig
+# Import routes
+from .routes import auth_bp, api_bp
 
 load_dotenv()
 
 def create_app():
     app = Flask(__name__)
+    # Register blueprints
+    app.register_blueprint(auth_bp)
+    app.register_blueprint(api_bp)
 
     # Load configuration based of FLASK_ENV environment variable
     env_name = os.getenv("FLASK_ENV")
-
     if env_name == 'production':
         app.config.from_object(ProductionConfig)
     elif env_name == 'development':
