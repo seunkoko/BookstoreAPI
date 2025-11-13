bde3818d65215e86a64f30a5ce8eb2b0291901fb
Oluwaseun Ilesanmi <seunowonikoko@gmail.com>
2025-11-13 12:01:36 -0500

    Login route implemented

--- bookstore_api/app/extensions.py
+++ bookstore_api/app/extensions.py
@@ -1,13 +1,9 @@
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_marshmallow import Marshmallow
-from sqlalchemy.orm import DeclarativeBase
 from flask_jwt_extended import JWTManager
 
-class Base(DeclarativeBase):
-  pass
-
-db = SQLAlchemy(model_class=Base)
+db = SQLAlchemy()
 migrate = Migrate()
 ma = Marshmallow()
 jwt = JWTManager()
