32eb316c57fd3219eab1c2435c4455c0c941fb8b
Oluwaseun Ilesanmi <seunowonikoko@gmail.com>
2025-11-13 12:05:51 -0500

    Tests jwt implementation

--- bookstore_api/app/routes/resources/users.py
+++ bookstore_api/app/routes/resources/users.py
@@ -1,6 +1,8 @@
 from flask_restful import Resource
+from flask_jwt_extended import jwt_required
 
 
 class UserListResource(Resource):
+    @jwt_required()
     def get(self):
         return {'users': {}}   
